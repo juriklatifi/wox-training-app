cc54da3100c35f970de61f7727be586ea6716c59
Jurik Latifi <jl53598@ubt-uni.net>
2024-03-27 19:03:17 +0100

    register form.

---
+++ src/pages/_app.js
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { useAuthStore } from "../context/authStore"
+
+const restrictedPathnames = ['/restricted']
+
+export default function App({Component, pageProps}) {
+    const router = useRouter()
+    const {authenticated, login, logout } = useAuthStore()
+
+    useEffect(() => {
+        const authStorage = JSON.parse(sessionStorage.getItem('auth-storage'))
+        const previousAuth = authStorage?.state?.authenticated
+        if(!authenticated && !previousAuth && restrictedPathnames.includes(router.pathname)) {
+            console.log('restriction failed redirecting to login!!')
+            router.push('/login')
+            return
+        }
+    }, [authenticated])
+    
+    return (
+        <Component {...pageProps} />
+    )
+}
